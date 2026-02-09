05c0970937d6b7ba3f7dc48f7d0cc77e49d0f12b
Ruben12MT <xrmartri675@ieshnosmachado.org>
2026-02-09 23:37:53 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__APP_CONFIG__ = {
-    API_URL: "http://35.171.137.18:3000/api",
-    UPLOADS_URL: "http://35.171.137.18:3000"
+    API_URL: "http://localhost:3000/api",
+    UPLOADS_URL: "http://localhost:3000"
 }
 
 // window.__APP_CONFIG__.API_URL
